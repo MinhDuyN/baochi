e36ae0cad6a33f85262199dbccd46b65b4741adf
minhd <minhd@ASUS>
2023-09-01 13:38:30 +0700

    add

---
+++ baochi_test/Components/IsHotView.cs
@@ -0,0 +1,18 @@
+﻿using baochi_test.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baochi_test.Components
+{
+    public class IsHotView : ViewComponent
+    {
+        private readonly BaochiTestContext _context;
+        public IsHotView(BaochiTestContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.BaiDangs.ToList());
+        }
+    }
+}
